e411fea5bd36d3e637f74350eaa96cac1115c7d6
Igor Sheviakov <isheviakov@rightandabove.com>
2019-02-11 00:28:33 +0200

    list of actions

--- app/Cli/Action/Help.php
+++ app/Cli/Action/Help.php
@@ -11,6 +11,15 @@ class Help implements Runable
     {
 
         echo 'Run cli/do.php <action> <param1> <param2> ...'.PHP_EOL;
+        echo 'List of actions:'.PHP_EOL;
+        $paths = scandir(__DIR__);
+        foreach($paths as $path)
+        {
+            if(preg_match('#^(\w+)\.php$#', $path, $matches))
+            {
+                echo '  > ' . strtolower($matches[1]) . PHP_EOL;
+            }
+        }
 
     }
 
