d1be56b7dac307a237e56af98e91e2eb6e15a9f0
Igor Sheviakov <isheviakov@rightandabove.com>
2019-02-18 00:51:30 +0200

    #mobile styles

--- views/layout/cloudbeds.php
+++ views/layout/cloudbeds.php
@@ -2,8 +2,10 @@
 <html>
 <head>
     <title>Cloudbeds range</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <script src="/js/cloudbeds/app.js"></script>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/app.css">   
 </head>
 <body>
     <?= $content ?>
